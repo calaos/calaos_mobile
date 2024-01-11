f2e5be976b8743a242e2863d38360a6e8aa2ef55
Raoul Hecky <raoul.hecky@gmail.com>
2024-01-11 12:00:54 +0100

    add support for modsettings driver instead of intel

--- docker/start.sh
+++ docker/start.sh
@@ -7,6 +7,14 @@ cleanup() {
 }
 trap 'cleanup' TERM EXIT
 
+# check if arguments --modesetting is set
+if [ "$1" = "--modesetting" ]; then
+    echo "[*] Using modesetting driver"
+    
+    #remove intel driver to enable modsetting
+    rm -f /usr/lib/xorg/modules/drivers/intel_drv.so
+fi
+
 /usr/bin/startx -- vt8 -keeptty -verbose 3 -logfile /dev/null &
 pid=$!
 
